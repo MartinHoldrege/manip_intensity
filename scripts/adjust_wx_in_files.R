c0f9a659bb6490ed921567aab20dea1963b0c206
Martin Holdrege <martinholdrege@gmail.com>
2021-04-06 20:46:50 -0600

    run with updated precipr functions. Now expected and simulated map from 200 sites is nearly identical.

--- scripts/adjust_wx_in_files.R
+++ scripts/adjust_wx_in_files.R
@@ -14,8 +14,7 @@
 
 library(rSOILWAT2) 
 library(tidyverse)
-library(precipr)
-source("scripts/functions.R")
+library(precipr) # my misc functions
 
 # read in observed map data -----------------------------------------------
 
@@ -60,66 +59,32 @@ compare_weather(ref_weather = as.matrix(wdata),
 # increase intensity ------------------------------------------------------
 
 # adjusting coeffs to double ppt intensity
-coeffs_2x <- precipr::adjust_mkv_in(mkv_in = coeffs, data = wdata,
+coeffs_2x <- precipr::adjust_coeffs(coeffs = coeffs, data = wdata,
                                     mean_mult = 2,
                                     adjust_sd = TRUE)
-# this issues suggests that both expected_ppt and adjust_mkv_in functions
-# have issues!
-expected_ppt(coeffs_2x); expected_ppt(coeffs)
+
+expected_ppt(coeffs_2x, adjust_for_truncnorm = TRUE)
+expected_ppt(coeffs, adjust_for_truncnorm = TRUE)
 # compare coeffs
 head(coeffs$mkv_doy)
 head(coeffs_2x$mkv_doy)
 
-
-wout2 <- dbW_generateWeather(x_empty, years = 2000:2500,
+wout2 <- dbW_generateWeather(x_empty, years = years,
                              wgen_coeffs = coeffs_2x)
 # this difference shows that expected_ppt still isn't being calculated
 # correctly
-calc_map(wout2); expected_ppt(coeffs_2x)  
+calc_map(wout2); expected_ppt(coeffs_2x, adjust_for_truncnorm = TRUE)  
 
-calc_map(wdata); expected_ppt(coeffs) # should be exactly(?) the same
+# note when compring to original data adjust_for_truncnorm = FALSE
+# gives closer match, b/ the wgen actually adds some biases
+calc_map(wdata); expected_ppt(coeffs) 
+
 compare_weather(ref_weather = as.matrix(wdata), 
                 weather = dbW_weatherData_to_dataframe(wout2),
                 N = 1, 
                 path = "figures/compare_wgen/", 
                 tag = "2x_intensity")
 
-
-# testing coeffs ----------------------------------------------------------
-
-# CONINTINUE here--note! increasing PPT_sd increases annual ppt! why?--because
-# negatives can be created by the normal distribution???, but aren't excepted?
-# if that is true, why is MAP correctly calculated for original PPT_avg and
-# PPT_sd values??
-
-# could it be cause be this code in SW_Markov.c?
-# /* Calculate Precipitation:
-#   prop = probability that it precipitates today depending on whether it
-# was wet (precipitated) yesterday `wetprob` or
-# whether it was dry yesterday `dryprob` */
-#   prob = (GT(*rain, 0.0)) ? SW_Markov.wetprob[doy0] : SW_Markov.dryprob[doy0];
-# 
-# p = RandUni(&markov_rng);
-# if (LE(p, prob)) {
-#   x = RandNorm(SW_Markov.avg_ppt[doy0], SW_Markov.std_ppt[doy0], &markov_rng);
-#   *rain = fmax(0., x);
-# } else {
-#   *rain = 0.;
-# }
-
-
-# testing effects of changing coeffs
-coeffs_test <- coeffs
-coeffs_test$mkv_doy$PPT_sd <- coeffs$mkv_doy$PPT_sd*2
-wout3 <- dbW_generateWeather(x_empty, years = years,
-                             wgen_coeffs = coeffs_test)
-calc_map(wout3)
-expected_ppt(coeffs_test)
-compare_weather(ref_weather = as.matrix(wdata), 
-                weather = dbW_weatherData_to_dataframe(wout3),
-                N = 1, 
-                path = "figures/compare_wgen/", 
-                tag = "test_intensity")
 
 # create coeffs for each site ---------------------------------------------
 
@@ -135,7 +100,7 @@ coeff_l <- map(sites, function(site) {
   # true MAP 
   map_obs <- wdata_df %>% 
     group_by(Year) %>% 
-    summarise(PPT_cm = sum(PPT_cm)) %>% 
+    summarise(PPT_cm = sum(PPT_cm), .groups = "drop") %>% 
     pull(PPT_cm) %>% 
     mean()
   
@@ -147,6 +112,8 @@ coeff_l <- map(sites, function(site) {
 })
 
 # calculating the mean z score of zero ppt (given the mean and sd for that day)
+# large z score suggests that many random draws would be negative and converted
+# to 0 in wgen
 z0 <- map_dbl(coeff_l, function(x) {
   z_vec <- (0 - x$mkv_doy$PPT_avg)/x$mkv_doy$PPT_sd # z score of 0
   z_vec[!is.finite(z_vec)] <- NA
@@ -168,7 +135,10 @@ map_obs <- map_dbl(coeff_l, function(x) x$map_obs) %>%
          site = sites)
 
 # expected map based on markov file and understanding of wgen
-map_obs$map_exp <- map_dbl(coeff_l, expected_ppt)
+# STOP: Continue here--problem is that for some NAN is being returen
+# form map_exp
+map_obs$map_exp <- map_dbl(coeff_l, expected_ppt, adjust_for_truncnorm = TRUE)
+
 
 map <- map_sim %>% 
   rename(map_sim = MAP) %>% 
@@ -177,50 +147,19 @@ map <- map_sim %>%
   mutate(z0 = z0,
          map_diff = map_sim - map_obs,
          map_diff_perc = map_diff/map_obs*100,
-         map_exp_diff = map_exp - map_obs)
+         map_exp_diff = map_exp - map_sim)
 
 
 hist(map$map_diff_perc)
 plot(map$map_diff_perc ~ map$z0)
 
-# simulated and expected map should be same, if expected_ppt function
-# were actually appropriately calculating expected ppt. it seems that it is 
-# not. 
+
+max(map$map_exp_diff) # the maximum difference between expected map
+# and simulated map is only 1/2 mm
 with(map, plot(map_exp~map_sim)); abline(0, 1)
+
+# difference between observed and simulate map are due to some bias
+# in the wgen
 with(map, plot(map_exp~map_obs)); abline(0, 1)
-with(map, hist((map_exp_diff)/map_obs*100)) # % difference between observed and expected map
-mean(map$map_diff)
-mean(map$map_diff_perc)
-median(map$map_diff)
-
-# site for which expected map farthest off
-filter(map, abs(map_exp_diff) == max(abs(map_exp_diff)))
-
-# CONTINUE--just trying to figure out what the difference is 
-# between coeffs were expected vals exactly match observed map
-# and those that don't
-
-good_exp <- map %>% 
-  filter(map_exp_diff == 0) %>% 
-  pull(site)
-
-coeff_l[[good_exp[1]]]$mkv_doy$PPT_avg %>% 
-  pull() %>% 
-  sum()
-
-check<- map_dbl(coeff_l, function(x) {
-  sum(x$mkv_doy$PPT_avg > 0 & x$mkv_doy$PPT_sd == 0)
-  
-})
-which(check == 0)
-good_exp
-coeff_l[[good_exp[1]]]$mkv_doy %>% View()
-View(coeff_l[[1]]$mkv_doy)
-# very strangely p_W_W for doy 366 is always 1 and p_W_D for doy 366 is always
-# 0, strange, not sure why! It's possible that it has something to do with 
-# summing across NAs?. Turns out that it is becuase in the weather
-# data base doy 366 is always just a repeat of doy 365. ie if 365 was
-# wet then 366 must also be wet. 
-
-map(coeff_l[1:10], function(x) tail(x$mkv_doy))
+with(map, plot(map_sim~map_obs)); abline(0, 1)
 
--- scripts/functions.R
+++ scripts/functions.R
@@ -149,24 +149,4 @@ doy2month <- function(year, doy) {
 
 
 
-# calculate map -----------------------------------------------------------
 
-
-calc_map <- function(wout) {
-  # calculate mean annual ppt
-  
-  # allowing dataframe or weatherdata list
-  df <- if (is.data.frame(wout)) {
-    wout
-  } else {
-    wout %>% 
-      rSOILWAT2::dbW_weatherData_to_dataframe() %>% 
-      as.data.frame() 
-  }
-  out <- df %>% 
-    group_by(.data$Year) %>% 
-    summarise(PPT_cm = sum(.data$PPT_cm), .groups = "drop") %>% 
-    pull(PPT_cm) %>% 
-    mean()
-  out
-}
